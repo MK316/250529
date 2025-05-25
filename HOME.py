060569002dda827191826d0d51749792dfd5156d
MK <99416359+MK316@users.noreply.github.com>
2025-05-25 17:54:58 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
+st.title("Grammar App (0529)")
+
 url="https://github.com/MK316/250529/raw/main/images/bg01-rose.jpg"
 st.image(url, caption="Grammar makes meaning clear.")
