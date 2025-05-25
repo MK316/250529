a4f0a558a8b6cf67364420d52c51929b0f1fd82d
MK <99416359+MK316@users.noreply.github.com>
2025-05-25 17:59:31 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("1학년 2반 Application (0529)")
+st.markdown("### 1학년 2반")
+st.markdown("#### 🐣 Applications (0529)")
 
 url="https://github.com/MK316/250529/raw/main/images/bg01-rose.jpg"
 st.image(url, caption="Grammar makes meaning clear.")
