cbef22cc210f54585d6fd87bf7f9ea7df8841729
MK <99416359+MK316@users.noreply.github.com>
2025-05-26 23:13:22 +0900

    Update 🍰D._Review_at_Home.py

--- pages/🍰D._Review_at_Home.py
+++ pages/🍰D._Review_at_Home.py
@@ -82,7 +82,7 @@ if "start_time" not in st.session_state:
     st.session_state["start_time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 if not st.session_state.name_entered:
-    user_name_input = st.text_input("시작하기 위해 아래 박스에 영문으로 이름을 쓰세요:", key="name_input")
+    user_name_input = st.text_input("시작하기 위해 아래 박스에 영문으로 이름을 쓰세요.", key="name_input")
     if st.button("Start Quiz") and user_name_input.strip():
         st.session_state.username = user_name_input.strip()
         st.session_state.name_entered = True
